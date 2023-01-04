d29435baa7f47f86318748bc350517232f5b6e66
Helio Frota <00hf11@gmail.com>
2023-01-04 11:55:23 -0300

    chore: using the latest jaeger version

--- scripts/start_new.sh
+++
@@ -1,7 +0,0 @@
-if [ ! -f "jaeger-1.40.0-linux-amd64.tar.gz" ]
-then 
-  wget https://github.com/jaegertracing/jaeger/releases/download/v1.40.0/jaeger-1.40.0-linux-amd64.tar.gz
-fi
-
-tar xf jaeger-1.40.0-linux-amd64.tar.gz
-./jaeger-1.40.0-linux-amd64/jaeger-all-in-one &
