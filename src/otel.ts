6ff8b0369396e51d548233abd2b3893b69883287
Helio Frota <00hf11@gmail.com>
2023-05-11 13:46:40 -0300

    refactor: we don't need sdk-metrics

--- src/otel.ts
+++ src/otel.ts
@@ -1,18 +1,13 @@
 // Let's keep this in case needed in the future.
 // import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
 
-import { NodeSDK } from '@opentelemetry/sdk-node';
+import { NodeSDK, metrics } from '@opentelemetry/sdk-node';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express';
 import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
 import { error } from 'console';
 import { Resource } from '@opentelemetry/resources';
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
-import {
-  MeterProvider,
-  PeriodicExportingMetricReader,
-} from '@opentelemetry/sdk-metrics';
-
 import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-http';
 
 // Let's keep this in case needed in the future.
@@ -23,14 +18,10 @@ const resource = new Resource({
 });
 
 const metricsExporter = new OTLPMetricExporter();
-const meterProvider = new MeterProvider({ resource: resource });
-meterProvider.addMetricReader(
-  new PeriodicExportingMetricReader({ exporter: metricsExporter })
-);
 
 const sdk = new NodeSDK({
   traceExporter: new OTLPTraceExporter(),
-  metricReader: new PeriodicExportingMetricReader({
+  metricReader: new metrics.PeriodicExportingMetricReader({
     exporter: metricsExporter,
     exportIntervalMillis: 3000,
   }),
